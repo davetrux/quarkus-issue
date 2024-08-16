a57c7645e7fa1e0c9f91184847b66e96f73f3410
trux <david.truxall@graphitehealth.io>
2024-08-16 10:37:30 -0400

    Updated so unit test will work

--- src/test/java/com/truxall/GreetingResourceTest.java
+++ src/test/java/com/truxall/GreetingResourceTest.java
@@ -1,12 +1,14 @@
 package com.truxall;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.security.TestSecurity;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
+@TestSecurity(authorizationEnabled = false)
 class GreetingResourceTest {
     @Test
     void testHelloEndpoint() {
